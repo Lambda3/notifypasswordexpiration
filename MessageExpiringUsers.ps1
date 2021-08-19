7f7f62ec3ac26ce1a89499efe5b1d18afca37f3d
Giovanni Bassi <giggio@giggio.net>
2021-08-19 09:51:41 -0300

    Make ad credentials optional

--- MessageExpiringUsers.ps1
+++ MessageExpiringUsers.ps1
@@ -15,19 +15,35 @@ Use a valid e-mail address.
 [CmdletBinding(DefaultParameterSetName = 'sendsmail')]
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingPlainTextForPassword', '', Justification = 'Will run in Docker, we can''t  pass a SecureString.')]
 param (
-    [Parameter(Mandatory)][string] $adUsername,
-    [Parameter(Mandatory)][string] $adPassword,
+    [Parameter(Mandatory, ParameterSetName = "providesauth_sendsmail")]
+    [Parameter(Mandatory, ParameterSetName = "providesauth_doesnotsendmail")]
+    [string] $adUsername,
+    [Parameter(Mandatory, ParameterSetName = "providesauth_sendsmail")]
+    [Parameter(Mandatory, ParameterSetName = "providesauth_doesnotsendmail")]
+    [string] $adPassword,
     [ValidatePattern('^((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])(\.([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]))*)$')]
     [string]$adDomain,
-    [Parameter(Mandatory)][string]$adSearchBase,
+    [Parameter(Mandatory)]
+    [string]$adSearchBase,
     [switch]$excludeExpiredUsers,
     [ValidatePattern('^((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])(\.([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]))*)$')]
-    [Parameter(Mandatory, ParameterSetName = "sendsmail")][string]$smtpServer,
-    [ValidateRange(1, [System.UInt16]::MaxValue)][Parameter(Mandatory, ParameterSetName = "sendsmail")][int]$smtpPort,
+    [Parameter(Mandatory, ParameterSetName = "providesauth_sendsmail")]
+    [Parameter(Mandatory, ParameterSetName = "sendsmail")]
+    [string]$smtpServer,
+    [ValidateRange(1, [System.UInt16]::MaxValue)]
+    [Parameter(Mandatory, ParameterSetName = "providesauth_sendsmail")]
+    [Parameter(Mandatory, ParameterSetName = "sendsmail")]
+    [int]$smtpPort,
     [ValidatePattern('^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$')]
-    [Parameter(Mandatory, ParameterSetName = "sendsmail")][string]$from,
-    [Parameter(ParameterSetName = "sendsmail")][string]$smtpUser,
-    [Parameter(Mandatory, ParameterSetName = "sendsmail")][string]$smtpPassword,
+    [Parameter(Mandatory, ParameterSetName = "providesauth_sendsmail")]
+    [Parameter(Mandatory, ParameterSetName = "sendsmail")]
+    [string]$from,
+    [Parameter(ParameterSetName = "providesauth_sendsmail")]
+    [Parameter(ParameterSetName = "sendsmail")]
+    [string]$smtpUser,
+    [Parameter(Mandatory, ParameterSetName = "providesauth_sendsmail")]
+    [Parameter(Mandatory, ParameterSetName = "sendsmail")]
+    [string]$smtpPassword,
     [ValidateScript( { Test-Path $_ -PathType Leaf })]
     [string]$emailBodyTemplateFile,
     [ValidateScript( { Test-Path $_ -PathType Leaf })]
@@ -39,12 +55,18 @@ param (
     [ValidatePattern('^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$')]
     [string]$singleRecipient,
     [switch]$noProgress,
-    [Parameter(ParameterSetName = "doesnotsendmail")][switch]$simulate
+    [Parameter(ParameterSetName = "providesauth_doesnotsendmail")]
+    [Parameter(ParameterSetName = "doesnotsendmail")]
+    [switch]$simulate
 )
 Set-StrictMode -Version 3.0
 
 function Get-MaxPasswordAge($user, $adCredential, $defaultmaxPasswordAge, $adDomain) {
-    $PasswordPol = Get-AduserResultantPasswordPolicy $user -Credential $adCredential -Server $adDomain
+    $commandExpression = 'Get-AduserResultantPasswordPolicy $user -Server $adDomain'
+    if ($adCredential) {
+        $commandExpression += ' -Credential $adCredential'
+    }
+    $PasswordPol = Invoke-Expression $commandExpression
     if ($null -ne $PasswordPol) {
         $maxPasswordAge = $PasswordPol.MaxPasswordAge
     } else {
@@ -59,7 +81,10 @@ if (!($adDomain)) {
     $adDomain = (Get-ADDomain -Current LocalComputer).Forest
 }
 
-$adCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $adUsername, (ConvertTo-SecureString $adPassword -AsPlainText -Force)
+if ($adUsername -and $adPassword) {
+    $adCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $adUsername, (ConvertTo-SecureString $adPassword -AsPlainText -Force)
+}
+
 if ($emailSubjectTemplateFile) {
     $subjectTemplate = Get-Content $emailSubjectTemplateFile -Raw -Encoding "utf8"
 } else {
@@ -100,13 +125,21 @@ if ($logFile) {
     }
 }
 
-$users = Get-ADUser -Server $adDomain -filter * -SearchBase $adSearchBase -properties Name, PasswordNeverExpires, PasswordExpired, PasswordLastSet, EmailAddress -Credential $adCredential `
+$getADUserExpression = "Get-ADUser -Server $adDomain -filter * -SearchBase '$adSearchBase' -properties Name, PasswordNeverExpires, PasswordExpired, PasswordLastSet, EmailAddress"
+if ($adCredential) {
+    $getADUserExpression += ' -Credential $adCredential'
+}
+$users = Invoke-Expression $getADUserExpression `
 | Where-Object { $_.Enabled -eq $true -and $_.PasswordNeverExpires -eq $false -and $null -ne $_.EmailAddress } `
 | Sort-Object -Property Name
 if ($excludeExpiredUsers) {
     $users = $users | Where-Object { $_.PasswordExpired -eq $false }
 }
-$defaultmaxPasswordAge = (Get-ADDefaultDomainPasswordPolicy -Server $adDomain -Credential $adCredential).MaxPasswordAge
+$getADDefaultDomainPasswordPolicyExpression = 'Get-ADDefaultDomainPasswordPolicy -Server $adDomain'
+if ($adCredential) {
+    $getADDefaultDomainPasswordPolicyExpression += ' -Credential $adCredential'
+}
+$defaultmaxPasswordAge = (Invoke-Expression $getADDefaultDomainPasswordPolicyExpression).MaxPasswordAge
 $today = Get-Date
 $formattedDate = Get-Date -Format yyyyMMdd
 
--- ShowExpiredUsers.ps1
+++ ShowExpiredUsers.ps1
@@ -5,8 +5,8 @@ Shows expired users
 [CmdletBinding(DefaultParameterSetName = 'sendsmail')]
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingPlainTextForPassword', '', Justification = 'Will run in Docker, we can''t  pass a SecureString.')]
 param (
-    [Parameter(Mandatory)][string] $adUsername,
-    [Parameter(Mandatory)][string] $adPassword,
+    [Parameter(Mandatory, ParameterSetName = "providesauth")][string] $adUsername,
+    [Parameter(Mandatory, ParameterSetName = "providesauth")][string] $adPassword,
     [ValidatePattern('^((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])(\.([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]))*)$')]
     [string]$adDomain,
     [Parameter(Mandatory)][string]$adSearchBase
@@ -19,9 +19,15 @@ if (!($adDomain)) {
     $adDomain = (Get-ADDomain -Current LocalComputer).Forest
 }
 
-$adCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $adUsername, (ConvertTo-SecureString $adPassword -AsPlainText -Force)
+if ($adUsername -and $adPassword) {
+    $adCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $adUsername, (ConvertTo-SecureString $adPassword -AsPlainText -Force)
+}
 
-$users = Get-ADUser -Server $adDomain -filter * -SearchBase $adSearchBase -properties Name, PasswordNeverExpires, PasswordExpired, PasswordLastSet, EmailAddress -Credential $adCredential `
+$getADUserExpression = "Get-ADUser -Server $adDomain -filter * -SearchBase '$adSearchBase' -properties Name, PasswordNeverExpires, PasswordExpired, PasswordLastSet, EmailAddress"
+if ($adCredential) {
+    $getADUserExpression += ' -Credential $adCredential'
+}
+$users = Invoke-Expression $getADUserExpression `
 | Where-Object { $_.Enabled -eq $true -and $_.passwordexpired -eq $true }
 
 Write-Output "Found $($users.Count) users expired."
